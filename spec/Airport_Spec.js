c2fa8b4a6893f94c9a949c3476d3c82324382fb2
Colin McCulloch <mccolane@gmail.com>
2016-01-05 15:14:44 +0000

    Class and method Defined

---
+++ spec/Airport_Spec.js
@@ -0,0 +1,12 @@
+describe("Airport", function() {
+  it("Airport exists", function() {
+    var airport = new Airport();
+  expect(airport).not.toBe(null);
+  });
+  it("Land method is defined", function() {
+      var airport = new Airport();
+      expect(airport.land).toBeDefined();
+  });
+
+
+});
